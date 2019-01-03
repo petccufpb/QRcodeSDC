8963a2abfbb96a77eac7e9a72b185ab62e59a9c0
Jorge Gomes <httpmelo@gmail.com>
2019-01-03 19:02:41 -0300

    personalizou a barra de navegação

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,16 +1,59 @@
 // tudo o que for ser renderizado, componente, precisa importar:
 import React from 'react';
-
+//
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 //os pontos representam as pastas
 import {Inicial, Perfil, Config} from '../viewer';
-// createStackNavigator : por onde criaremos nossa rota, que é por onde navegaremos no app
+// createMaterialTopTabNavigator : por onde criaremos nossa rota, que é por onde navegaremos no app
 
-import {createStackNavigator} from 'react-navigation';
+import {createMaterialTopTabNavigator, createAppContainer} from 'react-navigation';
 // criando a barra de navegação e criando cada um dos ícones(objetos) e passando como parâmetro o que vai ser o inicial
 
-const Rotas = createStackNavigator({
-  Inicial: { screen: Inicial }, Perfil: { screen: Perfil }, Configurações: { screen: Config }
-},{initialRouteName: 'Inicial'})
+const Stack = createMaterialTopTabNavigator({
+  Inicial: { screen: Inicial }, Perfil: { screen: Perfil }, Config: { screen: Config }
+},
+
+{
+  navigationOptions: ({ navigation }) => ({
+    tabBarIcon: ({ focused, tintColor }) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      if (routeName === 'Inicial') {
+        iconName = 'checkbox-marked-outline';
+      } else if (routeName === 'Perfil') {
+        iconName = 'account-location';
+      }else if(routeName === 'Config'){
+        iconName = 'settings-outline';
+      }
+
+      // You can return any component that you like here! We usually use an
+      // icon component from react-native-vector-icons
+      return <MaterialCommunityIcons name={iconName} size={25} color={'#fff'} />;
+    },
+  }),
+  tabBarOptions: {
+    activeTintColor: 'white',
+    inactiveTintColor: 'gray',
+    showIcon: true,
+    style: {
+      backgroundColor: '#161b1f',
+      elevation: 0,
+      borderBottomColor: 'gray',
+      borderBottomWidth: 1,
+    }, 
+    indicatorStyle: {
+      backgroundColor: 'white'
+    },
+    labelStyle: {
+      fontSize: 10
+    }
+  },
+}
+,
+{initialRouteName: 'Inicial'})
+
+
+const Rotas = createAppContainer(Stack);
 
 // aqui exportamos a barra inteira para outras classes importarem
 export default Rotas;
