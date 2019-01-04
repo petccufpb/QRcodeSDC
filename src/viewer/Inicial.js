44307d9b950e39207cb2082ac422e24cfbcb1e39
Jorge Gomes <httpmelo@gmail.com>
2019-01-04 18:41:18 -0300

    container dos dias criado e inseridos

--- src/viewer/Inicial.js
+++ src/viewer/Inicial.js
@@ -11,10 +11,11 @@ const styles = StyleSheet.create({
         padding: 16
     },
     containerDias: {
-        flex: 0.2,
+        height: 96,
         backgroundColor: "#0277BD",
         flexDirection: 'row',
-        padding: 5
+        padding: 5,
+        margin: 6
     },
     containerCirculo: {
         flex:0.3,
@@ -44,8 +45,21 @@ const styles = StyleSheet.create({
         color:"#0277BD"
     }
 });
+
 class Inicial extends React.Component{
-    
+
+//inicialização do construtor pai
+    constructor (props){      
+        super(props);
+//usando o this, ja cria o atributo  
+//array de objetos contendo a info      
+        this.dias = [{data: "11/02", texto: "Primeiro dia", id: "1"},
+                    {data: "12/02", texto: "Segundo dia", id: "2"},
+                    {data: "13/02", texto: "Terceiro dia", id: "3"},
+                    {data: "14/02", texto: "Quarto dia", id: "4S"},
+                    {data: "15/02", texto: "Quinto dia", id: "5"}];
+    } 
+
     static navigationOptions = {
         tabBarLabel: 'Inicial',
         tabBarIcon: ({ tintColor }) => (
@@ -57,21 +71,34 @@ class Inicial extends React.Component{
 
         return(
             <View style={styles.container}>
-                <View style={styles.containerDias}>
-                    <View style={styles.containerCirculo}>
-                        <View style={styles.estiloCirculo}>
-                            <Text style={styles.estiloTextoCirculo}>
-                                11/02
-                            </Text>    
-                        </View>    
-                    </View>
+                <FlatList
+                    //identificador de cada elemento
+                    keyExtractor={item => item.id}
+                    data={this.dias}
+                    renderItem={({item}) => (
+                
+                        <View style={styles.containerDias}>
+                            <View style={styles.containerCirculo}>
+                                <View style={styles.estiloCirculo}>
+                                    <Text style={styles.estiloTextoCirculo}>
+                                        {item.data}
+                                    </Text>    
+                                </View>    
+                            </View>
 
-                    <View style={styles.containerTexto}>
-                        <Text style={styles.estiloTexto}>
-                            Primeiro dia
-                        </Text>    
-                    </View>    
-                </View>
+                            <View style={styles.containerTexto}>
+                                <Text style={styles.estiloTexto}>
+                                    {item.texto}
+                                </Text>    
+                            </View>    
+                        </View>
+                    )}
+                />
+
+{/*                 
+                
+                */}                
+
             </View>    
         );
     }
